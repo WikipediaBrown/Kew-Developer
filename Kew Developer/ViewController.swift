dad9919e9d529f0f4a91e5ec2b4bd4f95de922d5
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2025-02-28 02:20:47 -0800

    Initial Commit

---
+++ Kew Developer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Kew Developer
+//
+//  Created by nonplus on 2/28/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Kew DeveloperTests/Kew_DeveloperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kew_DeveloperTests.swift
+//  Kew DeveloperTests
+//
+//  Created by nonplus on 2/28/25.
+//
+
+import Testing
+@testable import Kew_Developer
+
+struct Kew_DeveloperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
